d988e01e288a0ca0f4d51bd7ab5796b269bb1b4d
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-08-04 20:32:54 +0530

    Add files via upload

---
+++ Sparse_matrix.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define MAX 100
+
+struct spark_matrix {
+    int row;
+    int col;
+    int value;
+} Element;
+
+int main() {
+    int sparseMatrix[MAX][MAX];
+    Element elements[MAX];
+    int m, n, nonZeroCount = 0;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &m);
+    printf("Enter the number of columns: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &sparseMatrix[i][j]);
+            if (sparseMatrix[i][j] != 0) {
+                elements[nonZeroCount].row = i;
+                elements[nonZeroCount].col = j;
+                elements[nonZeroCount].value = sparseMatrix[i][j];
+                nonZeroCount++;
+            }
+        }
+    }
+
+    printf("\nSparse Matrix Representation (Row, Column, Value):\n");
+    for (int i = 0; i < nonZeroCount; i++) {
+        printf("%d %d %d\n", elements[i].row, elements[i].col, elements[i].value);
+    }
+
+    return 0;
+}
