952466d8aa85fe50c92a3ba92550288637272a0c
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-11-11 16:18:08 +0530

    Create insertion_sort.c

---
+++ 17_Insertion_Sort/insertion_sort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+// Function to perform insertion sort
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];      // Element to be inserted
+        int j = i - 1;
+        
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;  // Place the key element at its correct position
+    }
+}
+
+// Function to display the array
+void displayArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    // Take input for the number of elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];  // Declare an array of size n
+
+    // Take input for the elements of the array
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Perform insertion sort
+    insertionSort(arr, n);
+
+    // Display the sorted array
+    printf("Sorted array:\n");
+    displayArray(arr, n);
+
+    return 0;
+}
